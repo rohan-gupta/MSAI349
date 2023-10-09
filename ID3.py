87f7519fd3257e189cbdc6013bed0de1673beee5
Rohan Gupta <rohangupta1704@gmail.com>
2023-10-09 03:16:24 -0500

    Added wiring for individual components

--- ID3.py
+++ ID3.py
@@ -1,5 +1,6 @@
 from node import Node
 import math
+import json
 
 def ID3(examples, default):
   '''
@@ -8,6 +9,36 @@ def ID3(examples, default):
   and the target class variable is a special attribute with the name "Class".
   Any missing attributes are denoted with a value of "?"
   '''
+  root = ptr = Node()
+
+  if is_dataset_empty(examples):
+    return root
+
+  if is_dataset_positive(examples):
+    ptr.label = "+"
+    return root
+
+  if is_dataset_negative(examples):
+    ptr.label = "-"
+    return root
+
+  attributes = get_all_attributes(examples)
+  best_attribute = ""
+  best_attribute_information_gain = 0
+
+  for a in attributes:
+    temp = get_information_gain(dataset, a)
+    if temp > best_attribute_information_gain:
+      best_attribute = a
+      best_attribute_information_gain = temp
+
+  # split dataset
+
+  # create child nodes
+
+  # repeat process for each child node
+
+  return root
 
 def prune(node, examples):
   '''
@@ -28,16 +59,84 @@ def evaluate(node, example):
   assigns to the example.
   '''
 
+def is_dataset_empty(dataset):
+  pass
+
+def is_dataset_positive(dataset):
+  pass
+
+def is_dataset_negative(dataset):
+  pass
 
 def get_all_attributes(dataset):
-  pass
+  all_attributes = set()
 
-def get_most_common_target_class(dataset):
-  pass
+  for d in dataset:
+    for k in d:
+      if k == "Class":
+        continue
+      
+      all_attributes.add(k)
 
-def get_information_gain(dataset):
-  pass
+  return list(all_attributes)
+
+def get_information_gain(dataset, attribute):
+  sub_dataset = get_sub_datasets_by_attribute(dataset, attribute)
+  parent_entropy = get_entropy(dataset)
+  child_entropy = 0
+
+  for s in sub_dataset:
+    child_entropy += (len(s) / len(dataset)) * get_entropy(s)
+  
+  return parent_entropy - child_entropy
+  
+
+def get_sub_datasets_by_attribute(dataset, attribute):
+  sub_datasets = {}
+
+  for d in dataset:
+    if attribute not in d:
+      continue
+
+    if d[attribute] not in sub_dataset:
+      sub_datasets[d[attribute]] = []
+
+    sub_datasets[d[attribute]].append(d)
+
+  return list(sub_datasets.values())
+
 
 def get_entropy(dataset):
-  pass
+  target_class_probability = get_target_class_probability(dataset)
+  H = 0
 
+  for k, v in target_class_probability.items():
+    H += -1 * v * math.log2(v)
+
+  return H
+
+
+def get_target_class_frequencies(dataset):
+  target_class_frequencies = {}
+  
+  for d in dataset:
+    for k, v in d.items():
+      if k != "Class":
+        continue
+
+      if v not in target_class_frequencies:
+        target_class_frequencies[v] = 0
+
+      target_class_frequencies[v] += 1
+
+  return target_class_frequencies
+
+
+def get_target_class_probabilities(dataset):
+  target_class_frequencies = target_class_frequencies(dataset)
+  probabilities = {}
+
+  for k, v in target_class_frequencies.items():
+    probabilities[k] = v / len(dataset)
+
+  return probabilities
