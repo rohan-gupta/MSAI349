996fcba48739f497a043ae889b3a2d353b529309
Rohan Gupta <rohangupta1704@gmail.com>
2023-10-09 11:04:47 -0500

    Readded entropy gain calculator

--- ID3.py
+++ ID3.py
@@ -30,26 +30,62 @@ def evaluate(node, example):
   '''
 
 def get_information_gain(dataset, attribute):
-	sub_dataset = get_sub_datasets_by_attribute(dataset, attribute)
-	parent_entropy = get_entropy(dataset)
-	child_entropy = 0
+  sub_dataset = get_sub_datasets_by_attribute(dataset, attribute)
+  parent_entropy = get_entropy(dataset)
+  child_entropy = 0
 
-	for s in sub_dataset:
-		child_entropy += (len(s) / len(dataset)) * get_entropy(s)
-	
-	return parent_entropy - child_entropy
-	
+  for s in sub_dataset:
+    child_entropy += (len(s) / len(dataset)) * get_entropy(s)
+  
+  return parent_entropy - child_entropy
+  
 
 def get_sub_datasets_by_attribute(dataset, attribute):
-	sub_datasets = {}
+  sub_datasets = {}
 
-	for d in dataset:
-		if attribute not in d:
-			continue
+  for d in dataset:
+    if attribute not in d:
+      continue
 
-		if d[attribute] not in sub_dataset:
-			sub_datasets[d[attribute]] = []
+    if d[attribute] not in sub_dataset:
+      sub_datasets[d[attribute]] = []
 
-		sub_datasets[d[attribute]].append(d)
+    sub_datasets[d[attribute]].append(d)
 
-	return list(sub_datasets.values())
+  return list(sub_datasets.values())
+
+
+def get_entropy(dataset):
+  target_class_probability = get_target_class_probability(dataset)
+  H = 0
+
+  for k, v in target_class_probability.items():
+    H += -1 * v * math.log2(v)
+
+  return H
+
+
+def get_target_class_probabilities(dataset):
+  target_class_frequencies = target_class_frequencies(dataset)
+  probabilities = {}
+
+  for k, v in target_class_frequencies.items():
+    probabilities[k] = v / len(dataset)
+
+  return probabilities
+
+
+def get_target_class_frequencies(dataset):
+  target_class_frequencies = {}
+  
+  for d in dataset:
+    for k, v in d.items():
+      if k != "Class":
+        continue
+
+      if v not in target_class_frequencies:
+        target_class_frequencies[v] = 0
+
+      target_class_frequencies[v] += 1
+
+  return target_class_frequencies
