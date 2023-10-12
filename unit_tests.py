0410a2ca7b82a867cfaa528a88fefc862c3b55b7
JeongYoon-L <lee.jenny.cs@gmail.com>
2023-10-12 10:50:18 -0500

    minor adding to unit_test for visualization

--- unit_tests.py
+++ unit_tests.py
@@ -1,7 +1,9 @@
 import ID3, parse, random
+import matplotlib.pyplot as plt
 
 def testID3AndEvaluate():
   data = [dict(a=1, b=0, Class=1), dict(a=1, b=1, Class=1)]
+  # data = parse.parse("tennis.data")
   tree = ID3.ID3(data, 0)
   if tree != None:
     ans = ID3.evaluate(tree, dict(a=1, b=0))
@@ -60,7 +62,16 @@ def testID3AndTest():
 def testPruningOnHouseData(inFile):
   withPruning = []
   withoutPruning = []
+  training_accuracy = []
+  validation_accuracy = []
+  test_accuracy = []
+
+  prune_training_accuracy = []
+  prune_validation_accuracy = []
+  prune_test_accuracy = []
+
   data = parse.parse(inFile)
+  size_of_set = range(100)
   for i in range(100):
     random.shuffle(data)
     train = data[:len(data)//2]
@@ -70,18 +81,24 @@ def testPruningOnHouseData(inFile):
     tree = ID3.ID3(train, 'democrat')
     acc = ID3.test(tree, train)
     print("training accuracy: ",acc)
+    training_accuracy.append(acc)
     acc = ID3.test(tree, valid)
     print("validation accuracy: ",acc)
+    validation_accuracy.append(acc)
     acc = ID3.test(tree, test)
     print("test accuracy: ",acc)
+    test_accuracy.append(acc)
   
     ID3.prune(tree, valid)
     acc = ID3.test(tree, train)
     print("pruned tree train accuracy: ",acc)
+    prune_training_accuracy.append(acc)
     acc = ID3.test(tree, valid)
     print("pruned tree validation accuracy: ",acc)
+    prune_validation_accuracy.append(acc)
     acc = ID3.test(tree, test)
     print("pruned tree test accuracy: ",acc)
+    prune_test_accuracy.append(acc)
     withPruning.append(acc)
     tree = ID3.ID3(train+valid, 'democrat')
     acc = ID3.test(tree, test)
@@ -90,7 +107,19 @@ def testPruningOnHouseData(inFile):
   print(withPruning)
   print(withoutPruning)
   print("average with pruning",sum(withPruning)/len(withPruning)," without: ",sum(withoutPruning)/len(withoutPruning))
+
+  plt.figure(figsize=(8, 5))
+  plt.plot(size_of_set, training_accuracy, label='Training Accuracy')
+  plt.plot(size_of_set, validation_accuracy, label='Validation Accuracy')
+  # plt.plot(size_of_set, test_accuracy, label='Testing Accuracy')
+  plt.xlabel('size_of_set')
+  plt.ylabel('Accuracy')
+  plt.title('Training and Validation Accuracy vs. size_of_set')
+  plt.legend()
+  plt.grid(True)
+  plt.show()
   
 if __name__ == "__main__":
   # testID3AndEvaluate()
   testPruningOnHouseData("candy.data")
+  # testID3AndEvaluate()
